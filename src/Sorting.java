e1f963fc1c2cd003fed1541dea92da667d8e0d94
achmdabdillah <achmdabdil9@gmail.com>
2023-09-20 14:18:23 +0700

    change name to Sorting

---
+++ src/Sorting.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] arr = {1,2,0,4,5};
+
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap (int[] arr, int start, int end) {
+        int tmp = arr[end];
+        arr[end] = arr[start];
+        arr[start] = tmp;
+    }
+
+    static void bubble(int[] arr) {
+        boolean swap;
+        for(int i = 0; i < arr.length; i++) {
+            swap = false;
+            for(int j = 1; j < arr.length - i; j++) {
+                if(arr[j] < arr[j -1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j - 1]; 
+                    arr[j -1] = tmp;
+                    swap = true;
+                }
+            }
+            if(!swap){
+                break;
+            }
+        }
+    }
+}
